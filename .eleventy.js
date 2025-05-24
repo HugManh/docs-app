2c9268d92923ebaded1e8d3d2532fa54e1c86e9e
HugManh <manh2909@gmail.com>
2025-05-24 10:52:50 +0700

    chore: Initialize repo with quartz

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (eleventyConfig) {
-    return {
-        dir: {
-            input: "content", // Thư mục chứa Markdown
-            output: ".dist" // Thư mục chứa HTML
-        }
-    };
-};
